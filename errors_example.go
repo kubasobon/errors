39730c7ca6bfe593d061e2ba80ec655c3aa9f54a
kubasobon <wtty.fool@gmail.com>
2021-11-20 19:54:11 +0100

    fix import cycle

--- errors_example.go
+++ errors_example.go
@@ -2,25 +2,23 @@ package errors
 
 import (
 	"log"
-
-	"github.com/kubasobon/errors"
 )
 
 func ExampleMaskf() {
 	c := func() error {
-		return errors.New("If you see this - it's working")
+		return New("If you see this - it's working")
 	}
 
 	b := func() error {
 		if err := c(); err != nil {
-			return errors.Maskf(err, "b error")
+			return Maskf(err, "b error")
 		}
 		return nil
 	}
 
 	a := func() error {
 		if err := b(); err != nil {
-			return errors.Maskf(err, "a error")
+			return Maskf(err, "a error")
 		}
 		return nil
 	}
